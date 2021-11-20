121258628e0007a103eb06eb25db4ea1b2c8de7f
William Wolfram <willwolfram18@gmail.com>
2021-11-19 19:11:13 -0800

    Initial commit of example code

---
+++ src/example-library/src/Lib1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Lib1;
+public class Class1
+{
+    private readonly ILogger<Class1> _log;
+
+    public Class1(ILogger<Class1> log)
+    {
+        _log = log;
+    }
+
+    public void LogInfo(string message) => _log.LogInformation(message);
+}
---
+++ src/example-library/test/Lib1.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Lib1.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
